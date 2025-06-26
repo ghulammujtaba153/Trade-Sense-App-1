2edd6a862418770e45bb8bdd31042cd00cecdfc9
Mujtaba12-al <mujtaba@mativeinc.com>
2025-06-26 19:11:40 +0500

    url updated

--- src/configs/url.js
+++ src/configs/url.js
@@ -1,3 +1,6 @@
 // export const API_URL = "http://localhost:5000";
 
-export const API_URL = "https://trade-sense-app-backend.onrender.com"
+// export const API_URL = "https://trade-sense-app-backend.onrender.com"
+
+
+export const API_URL = "http://16.171.10.104"
