fe62d9afd6e527b5e3546052347f1c05ee370fa1
Mujtaba12-al <mujtaba@mativeinc.com>
2025-08-15 11:51:15 +0500

    url update

--- src/configs/url.js
+++ src/configs/url.js
@@ -2,6 +2,8 @@
 
 // export const API_URL = "https://trade-sense-app-backend.onrender.com"
 
-export const API_URL = "/api"
+// export const API_URL = "/api"
+
+export const API_URL = "https://test.trader365.space"
 
 // export const API_URL = "http://13.61.22.84"
